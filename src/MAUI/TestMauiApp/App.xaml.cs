f06472280c668be48649c545cf55ecdde91f4028
Juster.zhu <zhuzhen723723@163.com>
2023-03-07 23:16:02 +0800

    add MAUI test app

---
+++ src/MAUI/TestMauiApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TestMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/MAUI/TestMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestMauiApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
