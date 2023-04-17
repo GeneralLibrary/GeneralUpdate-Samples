8add9e6d33bab5e8445364887126786c66bb078d
Juster.zhu <zhuzhen723723@163.com>
2023-04-17 23:23:05 +0800

    add oss test

--- src/WPF/TestWpfApp/MainWindow.xaml.cs
+++ src/WPF/TestWpfApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestWpfApp.ViewModels;
 
 namespace TestWpfApp
 {
@@ -23,6 +24,7 @@ namespace TestWpfApp
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
--- src/WPF/TestWpfUpgradeApp/MainWindow.xaml.cs
+++ src/WPF/TestWpfUpgradeApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestWpfUpgradeApp.ViewModels;
 
 namespace TestWpfUpgradeApp
 {
@@ -23,6 +24,7 @@ namespace TestWpfUpgradeApp
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
