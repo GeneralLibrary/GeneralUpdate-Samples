a171dd247a42ebd943267b49feb6544a8c01a022
justerzhu <zhuzhen723723@outlook.com>
2024-11-20 14:11:53 +0800

    ad

---
+++ website/src/html/head.js
@@ -0,0 +1,11 @@
+// src/html/head.js
+import React from 'react';
+
+export default function Head() {
+  return (
+    <>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3450324887026774"
+        crossorigin="anonymous"></script>
+    </>
+  );
+}
