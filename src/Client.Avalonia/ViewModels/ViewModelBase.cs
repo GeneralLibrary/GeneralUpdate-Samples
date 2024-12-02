61e6ca336e404d4263eb9e4feb7e3a8c013f7277
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2024-12-02 23:18:53 +0800

    feat: initialize Client.Avalonia.

---
+++ src/Client.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Client.Avalonia.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ src/Client.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Client.Avalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
