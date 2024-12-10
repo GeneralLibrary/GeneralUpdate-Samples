a2938dc2c3f1f2a80e6fff7934d8d6966496e153
justerzhu <zhuzhen723723@outlook.com>
2024-12-10 17:39:59 +0800

    add Compress Sample

---
+++ src/Compress/CompressSample/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+using GeneralUpdate.Common.Compress;
+using GeneralUpdate.Common.Shared.Object;
+
+var source = @"D:\packet\release";
+var target = @"D:\packet\1.zip";
+CompressProvider.Compress(Format.ZIP,source,target, false, Encoding.UTF8);
+CompressProvider.Decompress(Format.ZIP,target,source, Encoding.UTF8);
+Console.WriteLine($"Done {File.Exists(target)}");
