1f5f59f60a35ea2e9682c54e9824e5bf5e4cee76
justerzhu <zhuzhen723723@outlook.com>
2024-12-10 17:06:14 +0800

    add oss sample

---
+++ src/OSS/OSSClientSample/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+using GeneralUpdate.ClientCore;
+using GeneralUpdate.Common.Shared.Object;
+
+var paramsOSS = new GlobalConfigInfoOSS();
+paramsOSS.Url = "http://192.168.50.203/versions.json";
+paramsOSS.CurrentVersion = "1.0.0.0";
+paramsOSS.VersionFileName = "versions.json";
+paramsOSS.AppName = "GeneralUpdate.Client.exe";
+paramsOSS.Encoding = Encoding.UTF8.WebName;
+GeneralClientOSS.Start(paramsOSS);
---
+++ src/OSS/OSSUpgradeSample/Program.cs
@@ -0,0 +1,11 @@
+﻿using GeneralUpdate.Core;
+
+/*
+ * GeneralUpdateOSS will by default read the JSON content of GlobalConfigInfoOSS stored in the system environment variables by GeneralClientOSS
+ * , and developers do not need to be concerned with the entire process.
+ * 
+ * Environment.GetEnvironmentVariable("GlobalConfigInfoOSS", EnvironmentVariableTarget.User);
+ * 
+ * Typically, GeneralClientOSS and GeneralUpdateOSS appear as a pair.
+ */
+GeneralUpdateOSS.Start();
