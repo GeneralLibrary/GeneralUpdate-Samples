9b2ff94da79ade9858ecc732fc9e47481afcb364
justerzhu <zhuzhen723723@outlook.com>
2024-12-10 16:49:44 +0800

    add diff sample

---
+++ src/Diff/Program.cs
@@ -0,0 +1,8 @@
+﻿using GeneralUpdate.Differential;
+
+var source = @"D:\packet\app";
+var target = @"D:\packet\release";
+var patch = @"D:\packet\patch";
+
+await DifferentialCore.Instance?.Clean(source, target, patch);
+await DifferentialCore.Instance?.Dirty(source, patch);
