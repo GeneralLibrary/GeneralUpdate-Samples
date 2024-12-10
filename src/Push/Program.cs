4fe92b439c5a9e64ae9fc754bb3c0b9e3da0e7cc
justerzhu <zhuzhen723723@outlook.com>
2024-12-10 16:46:26 +0800

    add push sample

---
+++ src/Push/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+using GeneralUpdate.ClientCore.Hubs;
+
+var hub = new UpgradeHubService("http://localhost:5000/UpgradeHub"
+    , null,"dfeb5833-975e-4afb-88f1-6278ee9aeff6");
+hub.AddListenerReceive((message) =>
+{
+    Debug.WriteLine(message);
+});
+await hub.StartAsync();
