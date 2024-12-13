492037064ae0c687c607b031432b476fb128e83e
闫驚鏵 <yanjinhuamail@163.com>
2024-12-13 21:46:51 +0800

    add in WD Upgrade

---
+++ UI/WPFDevelopers/Upgrade/Upgrade.WPF/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using Upgrade.WPF.Views;
+using MessageBox = WPFDevelopers.Controls.MessageBox;
+
+namespace Upgrade.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += MainWindow_Closing;
+        }
+
+        private void BtnUpgrade1_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            new UpgradeWindow().ShowDialog();
+        }
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("是否退出当前系统?", "询问", MessageBoxButton.OKCancel, MessageBoxImage.Question) != MessageBoxResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
