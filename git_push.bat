811dcdeb916ad599de8873d043cf55992217fc6d
justerzhu <zhuzhen723723@outlook.com>
2025-01-06 22:36:27 +0800

    Update git_push.bat

--- git_push.bat
+++ git_push.bat
@@ -3,6 +3,7 @@ echo Pushing to default remote repository...
 git push
 if %errorlevel% neq 0 (
     echo Failed to push to default remote repository.
+    pause
     exit /b %errorlevel%
 )
 
@@ -10,6 +11,7 @@ echo Pushing to upstream remote on 'main' branch...
 git push upstream main
 if %errorlevel% neq 0 (
     echo Failed to push to upstream remote on 'main' branch.
+    pause
     exit /b %errorlevel%
 )
 
@@ -17,6 +19,7 @@ echo Pushing to 'upstream_gitcode' remote on 'main' branch...
 git push upstream_gitcode main
 if %errorlevel% neq 0 (
     echo Failed to push to 'upstream_gitcode' remote on 'main' branch.
+    pause
     exit /b %errorlevel%
 )
 
