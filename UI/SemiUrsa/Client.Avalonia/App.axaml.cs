757b0afa772ee1ed2a71925ec8a0b7b25032054e
Zhang Dian <54255897+zdpcdt@users.noreply.github.com>
2026-08-16 16:35:31 +0800

    chore: update Avalonia and related package versions to 12.1.1 and clean up unused validation code.

--- UI/SemiUrsa/Client.Avalonia/App.axaml.cs
+++ UI/SemiUrsa/Client.Avalonia/App.axaml.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using Client.Avalonia.ViewModels;
 using Client.Avalonia.Views;
@@ -29,25 +26,9 @@ public partial class App : Application
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow();
         }
 
         base.OnFrameworkInitializationCompleted();
-    }
-
-    private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
-
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove)
-        {
-            BindingPlugins.DataValidators.Remove(plugin);
-        }
     }
 }
